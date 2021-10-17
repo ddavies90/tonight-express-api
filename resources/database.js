98aa1c29eb654394454649385361edbba9ee6885
David Davies <d.davies90@hotmail.co.uk>
2021-10-17 18:53:17 +0100

    Added config to db

--- resources/database.js
+++ resources/database.js
@@ -1,2 +1,8 @@
 const AWS = require('aws-sdk');
-const db = new AWS.DynamoDB();
+const db = new AWS.DynamoDB.DocumentClient({
+    region: 'eu-west-2',
+    credentials: {
+        accessKeyId: process.env.AKID,
+        secretAccessKey: process.env.SAK
+    }
+});
