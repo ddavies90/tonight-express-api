6fe92d30721e3469c8cf38eae82d1bd2d6ef365e
David Davies <d.davies90@hotmail.co.uk>
2021-10-17 16:52:25 +0100

    Tested locally:

--- api.js
+++ api.js
@@ -2,15 +2,16 @@ const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 const cors = require('cors');
-const jwtAuth = require('./src/middleware/jwtAuth');
 const apiRouter = require('./src/routes/apiRouter');
 const databaseRouter = require('./src/routes/databaseRouter');
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/user', jwtAuth, databaseRouter);
+app.use('/user', databaseRouter);
 app.use('/', apiRouter);
 
 
-module.exports.api = serverless(app);
+// module.exports.api = serverless(app);
+
+app.listen(3001, console.log('listening'));
--- src/routes/databaseRouter.js
+++ src/routes/databaseRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const checkJwt = require('../middleware/jwtAuth');
 const databaseController = require('../controllers/databaseController');
 
 router.route('/')
-    .get(databaseController.getFavourites)
+    .get(checkJwt, databaseController.getFavourites)
     // .post(jwtAuth, databaseController.addFavourite)
 
 module.exports = router;
